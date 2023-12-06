1d1f90904de2b6fb618a6563d404ed15f2dd4c40
Lucas Gabriel Tamanini Demori <lucas-demori2001@hotmail.com>
2023-12-06 10:28:14 -0300

    Fix last bugs

--- pages/anunciosUsuario.php
+++ pages/anunciosUsuario.php
@@ -20,11 +20,10 @@ if ($id == '') {
 
         if ($num_anuncios > 0) {
             while ($anuncio = mysqli_fetch_assoc($result)) {
-
         ?>
                 <div class="product_container mx-3 mt-4 mb-3 px-3 py-3 col-sm-11 col-md-10 col-lg-8">
                     <div class="product_image_container px-3">
-                        <img src="../assets/img/produto.png" alt="Imagem do produto">
+                        <img src="<?php echo explode(',', $anuncio['nome_img'])[0];?>" alt="Imagem do produto">
                     </div>
                     <div class="product_description_container ml-5 col-sm-7 col-md-7 col-lg-8">
                         <h3><?php echo $anuncio['nome_anuncio']; ?></h3>
@@ -53,7 +52,6 @@ if ($id == '') {
         function idAnuncio(responseId) {
             const idAnuncio = responseId;
             window.location.href = "editarAnuncio.php?id_anuncio=" + idAnuncio;
-
         }
     </script>
 </body>
